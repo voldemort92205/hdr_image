6053dde1fd141251e7c896948235074384c7bf27
voldemort92205 <voldemort92205@gmail.com>
2015-03-28 00:10:06 +0800

    main function

--- main.m
+++ main.m
@@ -6,8 +6,19 @@ close all
 
 filename = 'rain.png';
 I1 = imread(filename);
-
+[row, col, height] = size(I1);
 median = medianIntensity (I1);
 
+%convert to gray scale
 
-
+for i = 1:row
+	for j = 1:col
+		tmp = sum(double(I1(i, j, :))) / 3 ;
+		if tmp < median
+			I1(i, j, :) = 0;
+		else
+			I1(i, j, :) = 255;
+		end
+	end
+end
+imwrite(I1, 'test.png');
