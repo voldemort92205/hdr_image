bd50cbad34d768b383c21b9cf7324413855a64a2
voldemort92205 <voldemort92205@gmail.com>
2015-03-28 14:54:09 +0800

    solve some function

--- mysolve.m
+++ mysolve.m
@@ -1,53 +1,28 @@
-function [g, lnE] = mysolve(images, lamdba, weight)
-
-	[row, col, num] = size(images);
-	np = row*col;
-	n = 254;
-	A = zeros((np*num+n+1), np+n);
-	b = zeros((np+n+1), 1);
-
+function [g, lnE] = mysolve(images, B, lamdba, weight)
+	[row, num] = size(images);
+	n = 256;
+	A = zeros((row*num+n+1), row+n);
+	b = zeros(size(A,1), 1);
 	k = 1;
-	for i = 1:col
-		for j = 1:row
-			for k = 1:num
-				wij = weight(images(i, j, k)+1)
-			end
+	for j = 1:num
+		for i = 1:row
+			wij = weight(images(i,j)+1);
+			A(k,images(i,j)+1) = wij;
+			A(k,n+i) = -wij;
+			b(k+1) = wij * B(j);
+
+			k = k + 1;
 		end
 	end
-
-	
-	g = 0;
-	lnE = 0;
-
-
-
-%below is sample code from ppt
-%	n = 256;
-%	A = zeros(size(z,1) * size(z,2)+n+1, n+size(z,1));
-%	b = zeros(size(A,1), 1);
-
-%	k = 1;
-%	for i = 1:size(z,1)
-%		for j = 1:size(z,2)
-%			wij = w(z(i,j)+1);
-%			A(k,z(i,j)+1) = wij;
-%			A(k,n+i) = -wij;
-%			b(i,j) = wij * b(j);
-%			k = k+1;
-%		end
-%	end
-	
-%	A(k,129) = 1;
-%	k = k+1;
-
-%	for i = 1:n-2
-%		A(k,i) = l * w(i+1);
-%		A(k,i+1) = -2 * l * w(i+1);
-%		A(k,i+2) = l * w(i+1);
-%		k = k + 1;
-%	end
-
-%	x = A \ b;
-%	g = x(1:n);
-%	ln = x(n+1:size(x,1));
+	A(k,129) = 1;
+	k = k + 1;
+	for i = 1:n-2
+		A(k,i) = lamdba * weight(i+1);
+		A(k,i+1) = -2 * lamdba * weight(i+1);
+		A(k,i+2) = lamdba * weight(i+1);
+		k = k + 1;
+	end
+	x = A \ b;
+	g = x(1:n);
+	lnE = x(n+1:size(x,1));
 end
