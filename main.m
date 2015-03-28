86c7b582d56bd6bfafae2b9a2a9347bf0ac318b1
voldemort92205 <voldemort92205@gmail.com>
2015-03-28 19:47:23 +0800

    main function

--- main.m
+++ main.m
@@ -9,19 +9,28 @@ lambda = 1;
 weight = ones(256, 1);
 B = ones (256, 1);
 
-n = 1;
 pixel = 256;
 picked = 75;
 
 
 
 %use for loop to convert many image to gray scale
-filename = 'rain.png';
-%grayScale(filename, 'test.png');
-length = size(filename);
-pictures(1, 1:length(2)) = filename;
-
+[filename, savedname] = textread ('image.txt', '%s %s');
+n = size(filename, 1);
+for i = 1:n
+	tmp = grayScale(filename{i}, savedname{i});
+	[row, col] = size(tmp);
+	grayImages(i, 1:row, 1:col) = tmp;
+end
 %alias
+%record the range of each picture
+resize = zeros(n, 4);
+%for test
+
+for i = 1:(n-1)
+	[x, y] = alignment (grayImages(i,:, :), grayImages(i+1, :, :));
+end
+
 
 
 %calculage function
@@ -32,16 +41,22 @@ pictures(1, 1:length(2)) = filename;
 gf = zeros(3, pixel);
 lnE = zeros(3, picked);
 
-
-	I1 = imread (pictures(1,:));
+%read all images
+for i = 1:n
+	I1 = imread (filename{i});
 	[row, col, height] = size(I1);
 	range = row * col;
-	start = randi ([1, range-100], 1, 1);
+	images(i, 1:row, 1:col, 1:height) = I1;
+end
+start = randi ([1, range-100], 1, 1);
 
+
+
+%calculate g and ln(E)
 input = zeros(picked, n);
 for k = 1:3
 	for i = 1:n
-		tmp = I1(:, :, k);
+		tmp = images(i, :, :, k);
 		input(:, i) = tmp (start:(start+picked-1))';
 	end
 	[gf(k,:), lnE(k,:)] = mysolve(input, B, lambda, weight);	
