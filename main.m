396a1047a36ee9e608759711e19fa5b024ecaec8
voldemort92205 <voldemort92205@gmail.com>
2015-03-28 14:53:56 +0800

    main function

--- main.m
+++ main.m
@@ -3,7 +3,17 @@ close all
 
 
 %sample test is rain.jpg
+
+%const
+lambda = 1;
+weight = ones(256, 1);
+B = ones (256, 1);
+
 n = 1;
+pixel = 256;
+picked = 75;
+
+
 
 %use for loop to convert many image to gray scale
 filename = 'rain.png';
@@ -16,17 +26,27 @@ pictures(1, 1:length(2)) = filename;
 
 %calculage function
 %open image
-lambda = 1;
-weight = ones(256, 1);
-I1 = imread (pictures(1,:));
+
+
+%for r, g, b
+gf = zeros(3, pixel);
+lnE = zeros(3, picked);
+
+
+	I1 = imread (pictures(1,:));
+	[row, col, height] = size(I1);
+	range = row * col;
+	start = randi ([1, range-100], 1, 1);
+
+input = zeros(picked, n);
 for k = 1:3
 	for i = 1:n
-		input = I1(:, :, i);
+		tmp = I1(:, :, k);
+		input(:, i) = tmp (start:(start+picked-1))';
 	end
-	[gR, lnE] = mysolve(input, lambda, weight);
-		
+	[gf(k,:), lnE(k,:)] = mysolve(input, B, lambda, weight);	
 end
 
 
+%plot (1:pixel, gf(1, :), 1:pixel, gf(2, :), 1:pixel, gf(3,:));
 
-%[g, lnE] = mysolve (I1, )
