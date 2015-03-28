2a3784aaaf847648188fff6e8e3da754646e2fe2
voldemort92205 <voldemort92205@gmail.com>
2015-03-28 19:48:08 +0800

    alignment two pictures

---
+++ alignment.m
@@ -0,0 +1,32 @@
+function [x, y] = alignment (image1, image2)
+	
+	errorMax = 100;
+	[num1, row1, col1] = size(image1);
+	[num2, row2, col2] = size(image2);
+	row = max(row1, row2);
+	col = max(col1, col2);
+
+	%use for xor
+	picture1 = zeros(row1, col1);
+	picture2 = zeros(row2, col2);
+	tmp = find(image1(1,:,:) == 255);
+	picture1(tmp) = 1;
+	tmp = find(image2(1,:,:) == 255);
+	picture2(tmp) = 1;
+	
+
+	delta = 0;
+	%right shift
+	while range < (min(col, row)/2)
+		for j = 1:col-delta
+			
+		end
+	end
+	
+
+
+
+
+	x = 0;
+	y = 0;
+end
