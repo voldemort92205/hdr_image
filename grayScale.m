d10d738918b60ec7907cc75d6ae83e9713783728
voldemort92205 <voldemort92205@gmail.com>
2015-04-07 19:18:08 +0800

    no need these function, it will implemented in c++

--- grayScale.m
+++
@@ -1,8 +0,0 @@
-function [Image] = grayScale (input, output)
-	image = imread (input);
-	[row, col, height] = size(image);
-%	Image = (54 * double(image (:, :, 1)) + 183 * double(image (:, :, 2)) + 19 * double(image (:, :, 3))) / 256;
-	Image = rgb2gray(image);
-	Image = mtbFunction(Image);
-	imwrite (Image, output);
-end
